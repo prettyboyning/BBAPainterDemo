fd598846ff9df6deedcd1f85c1ce358895337100
ningliujie <ningliujie@baidu.com>
2019-01-26 20:54:08 +0800

    异步渲染view 优化

--- BBAPainterDemo/Classes/AsyncDraw/Draw/BBAPainterAsyncDrawView.h
+++ BBAPainterDemo/Classes/AsyncDraw/Draw/BBAPainterAsyncDrawView.h
@@ -8,6 +8,8 @@
 #import <UIKit/UIKit.h>
 #import "BBAPainterAsyncDrawLayer.h"
 
+typedef void(^BBAPainterAsyncDrawCallback)(BOOL drawInBackground);
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface BBAPainterAsyncDrawView : UIView
@@ -25,6 +27,9 @@ NS_ASSUME_NONNULL_BEGIN
 /// 下次AsyncDrawing完成前保留当前的contents
 @property (nonatomic, assign) BOOL reserveContentsBeforeNextDrawingComplete;
 
+// 是否永远使用离屏渲染，默认YES。子类如果不希望离屏渲染必须重写此方法并 重写drawingPolicy为BBAPainterViewDrawingPolicySynchronouslyDraw
+@property (nonatomic, assign, readonly) BOOL alwaysUsesOffscreenRendering;
+
 /// 绘制次数
 @property (nonatomic, assign, readonly) NSUInteger drawingCount;
 
@@ -33,6 +38,85 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// 异步绘制时global queue的优先级，默认优先级为DEFAULT。在设置了drawQueue时此参数无效。
 @property (nonatomic, assign) dispatch_queue_priority_t dispatchPriority;
+
+/**
+ * @brief 设置需要异步显示
+ */
+- (void)setNeedsDisplayAsync;
+
+/**
+ * @brief 如果可能，中断当前绘制工作
+ */
+- (void)interruptDrawingWhenPossible;
+
+/**
+ * @brief 设置异步绘制全局开关
+ *
+ * @param disable YES or NO
+ *
+ */
++ (void)setGlobalAsyncDrawingDisable:(BOOL)disable;
+
+/**
+ * @brief 是否全局禁用了异步绘制
+ *
+ */
++ (BOOL)globalAsyncDrawingDisabled;
+
+/**
+ * @brief 立即开始重绘流程，无需等到下一个runloop（异步绘制会在下个runloop开始）
+ */
+- (void)redraw;
+
+#pragma mark - Methods for subclass overriding
+
+/**
+ * @brief 子类可以重写，并在此方法中进行绘制，请勿直接调用此方法
+ *
+ * @param rect 进行绘制的区域，目前只可能是 self.bounds
+ * @param context 绘制到的context，目前在调用时此context都会在系统context堆栈栈顶
+ * @param asynchronously 当前是否是异步绘制
+ *
+ * @return 绘制是否已执行完成。若为 NO，绘制的内容不会被显示
+ *
+ */
+- (BOOL)drawInRect:(CGRect)rect withContext:(CGContextRef)context asynchronously:(BOOL)asynchronously;
+
+/**
+ * @brief 子类可以重写，并在此方法中进行绘制，请勿直接调用此方法
+ *
+ * @param rect 进行绘制的区域，目前只可能是 self.bounds
+ * @param context 绘制到的context，目前在调用时此context都会在系统context堆栈栈顶
+ * @param asynchronously 当前是否是异步绘制
+ * @param userInfo 由currentDrawingUserInfo传入的字典，供绘制传参使用
+ *
+ * @return 绘制是否已执行完成。若为 NO，绘制的内容不会被显示
+ */
+- (BOOL)drawInRect:(CGRect)rect withContext:(CGContextRef)context asynchronously:(BOOL)asynchronously userInfo:(NSDictionary *)userInfo;
+
+/**
+ * @brief 子类可以重写，是绘制即将开始前的回调，请勿直接调用此方法
+ *
+ * @param asynchronously 当前是否是异步绘制
+ */
+- (void)drawingWillStartAsynchronously:(BOOL)asynchronously;
+
+/**
+ * @brief 子类可以重写，是绘制完成后的回调，请勿直接调用此方法
+ *
+ * @param asynchronously 当前是否是异步绘制
+ * @param success 绘制是否成功
+ *
+ * @discussion 如果在绘制过程中进行一次重绘，会导致首次绘制不成功，第二次绘制成功。
+ */
+- (void)drawingDidFinishAsynchronously:(BOOL)asynchronously success:(BOOL)success;
+
+/**
+ * @brief 子类可以重写，用于在主线程生成并传入绘制所需参数
+ *
+ * @discussion 有时在异步线程配置参数可能导致crash，例如在异步线程访问ivar。可以通过此方法将参数放入字典并传入绘制方法。此方法会在displayLayer:的当前线程调用，一般为主线程。
+ */
+- (NSDictionary *)currentDrawingUserInfo;
 
 @end
 
--- BBAPainterDemo/Classes/AsyncDraw/Draw/BBAPainterAsyncDrawView.m
+++ BBAPainterDemo/Classes/AsyncDraw/Draw/BBAPainterAsyncDrawView.m
@@ -66,6 +66,220 @@ static BOOL _globalAsyncDrawDisabled = NO;
     }
 }
 
+#pragma mark - draw
+
+- (void)drawRect:(CGRect)rect {
+    [self drawingWillStartAsynchronously:NO];
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    if (!context) {
+        NSLog(@"may be memory warning");
+    }
+    
+    [self drawInRect:self.bounds withContext:context asynchronously:NO userInfo:[self currentDrawingUserInfo]];
+    [self drawingDidFinishAsynchronously:NO success:YES];
+}
+
+- (void)setNeedsDisplay {
+    [self.layer setNeedsDisplay];
+}
+
+- (void)setNeedsDisplayInRect:(CGRect)rect {
+    [self.layer setNeedsDisplayInRect:rect];
+}
+
+- (void)displayLayer:(CALayer *)layer {
+    if (!layer) return;
+    
+    NSAssert([layer isKindOfClass:[BBAPainterAsyncDrawLayer class]], @"WMGAsyncDrawingView can only display BBAPainterAsyncDrawLayer");
+    
+    if (layer != self.layer) return;
+    
+    [self _displayLayer:(BBAPainterAsyncDrawLayer *)layer rect:self.bounds drawingStarted:^(BOOL drawInBackground) {
+        [self drawingWillStartAsynchronously:drawInBackground];
+    } drawingFinished:^(BOOL drawInBackground) {
+        [self drawingDidFinishAsynchronously:drawInBackground success:YES];
+    } drawingInterrupted:^(BOOL drawInBackground) {
+        [self drawingDidFinishAsynchronously:drawInBackground success:NO];
+    }];
+}
+
+- (void)_displayLayer:(BBAPainterAsyncDrawLayer *)layer
+                 rect:(CGRect)rectToDraw
+       drawingStarted:(BBAPainterAsyncDrawCallback)startCallback
+      drawingFinished:(BBAPainterAsyncDrawCallback)finishCallback
+   drawingInterrupted:(BBAPainterAsyncDrawCallback)interruptCallback {
+    BOOL drawInBackground = layer.isAsyncDrawsCurrentContent && ![[self class] globalAsyncDrawingDisabled];
+    
+    [layer increaseDrawingCount];
+    
+    NSUInteger targetDrawingCount = layer.drawingCount;
+    
+    NSDictionary *drawingUserInfo = [self currentDrawingUserInfo];
+    
+    void (^drawBlock)(void) = ^{
+        
+        void (^failedBlock)(void) = ^{
+            if (interruptCallback) {
+                interruptCallback(drawInBackground);
+            }
+        };
+        
+        if (layer.drawingCount != targetDrawingCount) {
+            failedBlock();
+            return;
+        }
+        
+        CGSize contextSize = layer.bounds.size;
+        BOOL contextSizeValid = contextSize.width >= 1 && contextSize.height >= 1;
+        CGContextRef context = NULL;
+        BOOL drawingFinished = YES;
+        
+        if (contextSizeValid) {
+            UIGraphicsBeginImageContextWithOptions(contextSize, layer.isOpaque, layer.contentsScale);
+            
+            context = UIGraphicsGetCurrentContext();
+            
+            if (!context) {
+                NSLog(@"may be memory warning");
+            }
+            
+            CGContextSaveGState(context);
+            
+            if (rectToDraw.origin.x || rectToDraw.origin.y) {
+                CGContextTranslateCTM(context, rectToDraw.origin.x, -rectToDraw.origin.y);
+            }
+            
+            if (layer.drawingCount != targetDrawingCount) {
+                drawingFinished = NO;
+            } else {
+                drawingFinished = [self drawInRect:rectToDraw withContext:context asynchronously:drawInBackground userInfo:drawingUserInfo];
+            }
+            
+            CGContextRestoreGState(context);
+        }
+        
+        // 所有耗时的操作都已完成，但仅在绘制过程中未发生重绘时，将结果显示出来
+        if (drawingFinished && targetDrawingCount == layer.drawingCount)
+        {
+            CGImageRef CGImage = context ? CGBitmapContextCreateImage(context) : NULL;
+            
+            {
+                // 让 UIImage 进行内存管理
+                UIImage *image = CGImage ? [UIImage imageWithCGImage:CGImage] : nil;
+                
+                void (^finishBlock)(void) = ^{
+                    
+                    // 由于block可能在下一runloop执行，再进行一次检查
+                    if (targetDrawingCount != layer.drawingCount)
+                    {
+                        failedBlock();
+                        return;
+                    }
+                    
+                    layer.contents = (id)image.CGImage;
+                    
+                    [layer setContentsChangedAfterLastAsyncDrawing:NO];
+                    [layer setReserveContentsBeforeNextDrawingComplete:NO];
+                    if (finishCallback)
+                    {
+                        finishCallback(drawInBackground);
+                    }
+                    
+                    // 如果当前是异步绘制，且设置了有效fadeDuration，则执行动画
+                    if (drawInBackground && layer.fadeDuration > 0.0001)
+                    {
+                        layer.opacity = 0.0;
+                        
+                        [UIView animateWithDuration:layer.fadeDuration delay:0.0 options:UIViewAnimationOptionAllowUserInteraction animations:^{
+                            layer.opacity = 1.0;
+                        } completion:NULL];
+                    }
+                };
+                
+                if (drawInBackground)
+                {
+                    dispatch_async(dispatch_get_main_queue(), finishBlock);
+                }
+                else
+                {
+                    finishBlock();
+                }
+            }
+            
+            if (CGImage) {
+                CGImageRelease(CGImage);
+            }
+        }
+        else
+        {
+            failedBlock();
+        }
+        
+        UIGraphicsEndImageContext();
+    };
+    
+    if (startCallback)
+    {
+        startCallback(drawInBackground);
+    }
+    
+    if (drawInBackground)
+    {
+        // 清空 layer 的显示
+        if (!layer.reserveContentsBeforeNextDrawingComplete)
+        {
+            layer.contents = nil;
+        }
+        
+        dispatch_async([self drawQueue], drawBlock);
+    }
+    else
+    {
+        void (^block)(void) = ^{
+            @autoreleasepool {
+                drawBlock();
+            }
+        };
+        
+        if ([NSThread isMainThread])
+        {
+            // 已经在主线程，直接执行绘制
+            block();
+        }
+        else
+        {
+            // 不应当在其他线程，转到主线程绘制
+            dispatch_async(dispatch_get_main_queue(), block);
+        }
+    }
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector {
+    if (!self.alwaysUsesOffscreenRendering) {
+        // 此方法在 -[super initWithFrame:frame] 时检查，因此必须通过重写保证此时的drawingPolicy已设置正确
+        if ([NSStringFromSelector(aSelector) isEqual:@"displayLayer:"]) {
+            return self.drawingPolicy != BBAPainterViewDrawingPolicySyncDraw;
+        }
+    }
+    return [super respondsToSelector:aSelector];
+}
+
+#pragma mark - public
+
+- (void)setNeedsDisplayAsync {
+    self.contentsChangedAfterLastAsyncDrawing = YES;
+    [self setNeedsDisplay];
+}
+
+- (void)redraw {
+    [self displayLayer:self.layer];
+}
+
+- (BOOL)alwaysUsesOffscreenRendering {
+    return YES;
+}
+
 #pragma mark - private
 
 - (void)interruptDrawingWhenPossible {
@@ -79,7 +293,37 @@ static BOOL _globalAsyncDrawDisabled = NO;
     return dispatch_get_global_queue(self.dispatchPriority, 0);
 }
 
+#pragma mark - Methods for subclass overriding
+
+- (NSDictionary *)currentDrawingUserInfo {
+    return nil;
+}
+
+- (void)drawingWillStartAsynchronously:(BOOL)asynchronously {
+    
+}
+
+- (BOOL)drawInRect:(CGRect)rect withContext:(CGContextRef)context asynchronously:(BOOL)asynchronously {
+    return YES;
+}
+
+- (BOOL)drawInRect:(CGRect)rect withContext:(CGContextRef)context asynchronously:(BOOL)asynchronously userInfo:(NSDictionary *)userInfo {
+    return [self drawInRect:rect withContext:context asynchronously:asynchronously];
+}
+
+- (void)drawingDidFinishAsynchronously:(BOOL)asynchronously success:(BOOL)success {
+    
+}
+
 #pragma mark - getter and setter
+
++ (BOOL)globalAsyncDrawingDisabled {
+    return _globalAsyncDrawDisabled;
+}
+
++ (void)setGlobalAsyncDrawingDisable:(BOOL)disable {
+    _globalAsyncDrawDisabled = disable;
+}
 
 - (void)setDrawingPolicy:(BBAPainterViewDrawingPolicy)drawingPolicy {
     _drawingLayer.drawingPolicy = drawingPolicy;
@@ -123,5 +367,7 @@ static BOOL _globalAsyncDrawDisabled = NO;
     }
     _dispatchDrawQueue = dispatchDrawQueue;
 }
+
+
 
 @end
